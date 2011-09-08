e44c86e2d86dc86ff4f2fd3ff3fbddbbc48f9ba4
Josiah Ritchie <josiah.ritchie@gmail.com>
2011-09-08 11:20:44 -0400

    adding the ansi2html email script and some modifications that move the headers into the script to make it easier to modify this information

--- task-ansi2email.sh
+++ task-ansi2email.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
 #email myself info about upcoming tasks in a pretty HTML format
-# Requires ssmtp
+# Requires ssmtp and ansi2html script
 # cron: 30 5 * * 1-6 /home/user/bin/taskwarrior-notifications/task-email.sh
 
 # Directory to find support files
 inc="/home/jritchie/scripts/taskwarrior-notifications"
 tmp_email="/tmp/task_email.txt"
-templates="$inc/templates"
 scripts="$inc/scripts"
 # Whom to email this too
 sendto="josiah@fim.org"
 
-# Format time string
-date=`date '+%b %d'` #Sept 07
-
+#setup headers so the HTML shows properly
 cat > $tmp_email <<EOF
 From: The Task List <$sendto> 
 To: $sendto
@@ -24,11 +21,8 @@ Content-Type: text/html
 Content-Disposition: inline
 EOF
 
-# Get the template loaded up
-cat $templates/html_email_head.template >> $tmp_email
-echo "<h1>Task Info: $date</h1>" >> $tmp_email
-echo `task _query status:completed | $scripts/export-html.py` >> $tmp_email
-cat $templates/html_email_foot.template >> $tmp_email
+#pump the task information into the email
+task status:completed | $scripts/ansi2html.sh >> $tmp_email
 
 # Send the email
 ssmtp $sendto < $tmp_email
